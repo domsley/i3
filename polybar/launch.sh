ee3d274d487fb5fef1adb621b5bb445484ef86f9
Dominik Matousek <matousek.dominik@icloud.com>
2020-12-06 03:18:01 +0100

    New polybar theme and i3 config update.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
-killall -q i3bar
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar main &
-
-echo "Polybar launched..."
+# Launch bar1 and bar2
+polybar main -c ~/.config/polybar/config.ini &
